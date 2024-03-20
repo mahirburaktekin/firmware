bf329f7ceb585b45390a017fedcffb5ff3679a16
mahirburaktekin <mahirburaktekin@protonmail.com>
2024-03-21 00:23:17 +0300

    Initial commit

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022 Paranoid Android
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),lisa)
+
+$(info Including firmware for lisa...)
+
+FIRMWARE_IMAGES := $(wildcard $(LOCAL_PATH)/images/*)
+
+$(foreach f, $(notdir $(FIRMWARE_IMAGES)), \
+    $(call add-radio-file,images/$(f)))
+
+endif
+
